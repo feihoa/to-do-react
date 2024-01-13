7b95b04e2331052d7db875c270d650435e7f408f
Мария Терентьева <MTerentyeva@rpa-robin.ru>
2024-01-14 01:35:35 +0300

    additions

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+        </Route>
+        
+        <Route path="e" element={<h2>Главная</h2>} />
+          <Route path="about" element={<h2>J</h2>} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
